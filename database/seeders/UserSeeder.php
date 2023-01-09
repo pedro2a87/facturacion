7482e0684134e6d680c2fd0caa075b5d9e050a04
Pedro Arredondo <arredondopedro87@gmail.com>
2023-01-09 15:50:07 -0400

    UserSeeder Actualizado

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,9 +18,17 @@ class UserSeeder extends Seeder
         $user = User::create([
             'name' => 'Admin',
             'email' => 'admin@facturacion.test',
-            'password' => Hash::make('@dm1n'),
+            'password' => Hash::make('@dm1n123'),
         ]);
         $user->assignRole(1);
+
+        $user = User::create([
+            'name' => 'Pedro Arredondo',
+            'email' => 'pedro@facturacion.test',
+            'password' => Hash::make('@pedro123'),
+        ]);
+        $user->assignRole(2);
+
         User::factory(500)->create()->each(function ($item, $key) {
             $item->assignRole(2);
         });
